1cae8a223c360682340644a7e71fa294d7c91c25
Matthew Chuchul <matthewchuchul@matthew-chuchuls-macbook-pro.local>
2015-06-17 21:37:08 -0400

    add form validation and error styles

--- js/main.js
+++ js/main.js
@@ -14,4 +14,67 @@ $(function() {
     }
   });
 
+  ///// Contact Form Validation //////////
+
+  function  validateName(fullname) {
+    if(fullname.length > 2) {
+      $("#fullname").removeClass('error');
+      $('#fullname-error').hide();
+      return true;
+
+    }
+    else {
+      $("#fullname").addClass('error');
+      $('#fullname-error').show();
+      return false;
+
+    }
+  }
+
+function  validateEmail(email) {
+    var re = //
+    if(re.test(email)) {
+      $("#email").removeClass('error');
+      $('#email-error').hide();
+      return true;
+    }
+    else {
+      $("#email").addClass('error');
+      $('#email-error').show();
+      return false;
+
+    }
+  }
+
+  function  validateMessage(message) {
+    if(message.lenght > 0) {
+      $("#mesage").removeClass('error');
+      $('#message-error').hide();
+      return true;
+    }
+    else {
+      $("#message").addClass('error');
+      $('#message-error').show();
+      return false;
+
+    }
+  }
+
+  $('form').submit(function(event){
+
+    var fullname = $('#fullname').val(),
+        email = $('#email').val(),
+        message = $.trim($('#message').val());
+
+    if (validateName(fullname) & validateEmail(email) & validateMessage(message)) {
+
+    }
+    else {
+
+    }
+
+    event.preventDefault(); // prevent form from submitting
+
+  });
+
 });
